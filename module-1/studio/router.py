72c0b1cf5bb34d95b08bec5f84e6896b247e513d
Ahmed <re.90@live.com>
2025-07-29 21:54:23 +0530

    adapt router to use openai compatible LLMs - OK

--- module-1/studio/router.py
+++ module-1/studio/router.py
@@ -1,7 +1,12 @@
-from langchain_openai import ChatOpenAI
+import os 
 from langgraph.graph import MessagesState
 from langgraph.graph import StateGraph, START, END
 from langgraph.prebuilt import ToolNode, tools_condition
+
+from langchain_sambanova import ChatSambaNovaCloud
+from dotenv import load_dotenv
+# Load environment variables
+load_dotenv()
 
 # Tool
 def multiply(a: int, b: int) -> int:
@@ -14,8 +19,25 @@ def multiply(a: int, b: int) -> int:
     return a * b
 
 # LLM with bound tool
-llm = ChatOpenAI(model="gpt-4o")
-llm_with_tools = llm.bind_tools([multiply])
+# Define LLM with bound tools
+
+api_key = os.getenv("SAMBANOVA_API_KEY")
+if not api_key:
+    raise ValueError("SAMBANOVA_API_KEY environment variable is not set.")
+
+
+samba_api_key = os.getenv("SAMBANOVA_API_KEY")
+
+my_llm = ChatSambaNovaCloud(
+    sambanova_api_key=samba_api_key,
+    # model="Llama-4-Maverick-17B-128E-Instruct",
+    model="DeepSeek-V3-0324",
+    max_tokens=4096,
+    temperature=0.0,
+    top_p=0.01,
+)
+
+llm_with_tools = my_llm.bind_tools([multiply])
 
 # Node
 def tool_calling_llm(state: MessagesState):
